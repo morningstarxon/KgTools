e0a19a3bcbab56e3452ad8650c3625ee457dc7fe
Ael'Astur <morningstarxon@gmail.com>
2026-05-09 20:25:31 -0700

    Update and rename KgKeys.py to KeyLogger/Keys-V1.1.py

--- KeyLogger/Keys-V1.1.py
+++ KeyLogger/Keys-V1.1.py
@@ -1,7 +1,7 @@
 from pynput import keyboard
 
 def on_press(key):
-    # Exact name! BTW ctrl & other keys show up as text, not a ctrl or smth
+ # Exact name! BTW ctrl & other keys show up as text, not a ctrl or smth
     print(f"Key detected: {key}")
 
 print("You can press keys. If you are debugging, try not to spam keys or it'll be hard to find the one you want")
